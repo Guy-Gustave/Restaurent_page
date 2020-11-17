d7e029b1994cd0a1c0c251d2dd959f2314ea274d
Guy <guymoustapha@gmail.com>
2020-11-17 17:41:32 +0200

    fix linters errors

--- src/home.js
+++ src/home.js
@@ -1,21 +1,18 @@
 function home() {
-  let content =  document.getElementById('content');
-  content.innerHTML= "";
-  let containerh =  document.createElement('div');
-  let bookButton = document.createElement('button');
-  containerh.setAttribute("id", "home-container");
+  const content = document.getElementById('content');
+  content.innerHTML = '';
+  const containerh = document.createElement('div');
+  const bookButton = document.createElement('button');
+  containerh.setAttribute('id', 'home-container');
   bookButton.setAttribute('id', 'reserve-button');
-
-  let message = document.createElement('p');
+  const message = document.createElement('p');
   message.setAttribute('id', 'accuei-message');
   message.textContent = 'the restaurent that cares yur choice, the food of al season ';
   containerh.appendChild(message);
-  bookButton.textContent= 'order your favourite dish'
+  bookButton.textContent = 'order your favourite dish';
 
   content.appendChild(containerh);
   content.appendChild(bookButton);
+}
 
-  
-};
-
-export {home};
+export default { home };
